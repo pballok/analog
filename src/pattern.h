011641ffeac53dd3eb37a57bf2dadf888521b8a9
pballok <pballok@gmail.com>
2010-11-09 17:19:16 +0100

    Started API documentation for Action module

--- src/pattern.h
+++ src/pattern.h
@@ -6,11 +6,36 @@
 #include <QDomElement>
 #include <QRegExp>
 
+//! Represents a Pattern (regular expression) used to identify actions in the input log file.
+
+/*!
+ * cPattern is basically a regular expression that can match the input log lines.
+ * It has a unique Name, so Action Definitions can refer to what pattern(s) they
+ * consist of. It uses PCRE compatible regular expression, and can capture parts
+ * of the matched text.
+ */
+
 class cPattern
 {
 public:
+    //! \brief Empty constructor
+    /*!
+     *
+     * The default (empty) constructor. It only initialises the member
+     * variables with default values.
+     */
     cPattern();
+
+    //! \brief Alternate constructor to initialize from XML Node
+    /*!
+     * \param p_poElem pointer to a "pattern" XML Node Element
+     * \sa cPattern()
+     * A constructor that initialises the member variables from a "pattern"
+     * XML Node Element.
+     */
     cPattern( const QDomElement *p_poElem );
+
+    //! \brief Destructor
     ~cPattern();
 
     QString      name() const throw();
